5809ec0b135a67bc13da95149745c14089507587
MaxwellSorenGottesman <maxgman16@yahoo.com>
2023-01-13 23:44:08 -0500

    commit tree 8775fbf0e8ed3fff00b80758474edcfa97003059

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [listItems, setListItems] = useState(items);
+
+  function applyFilter(e) {
+    setListItems(items.filter(cv => cv.category === e.target.value));
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(e) => applyFilter(e)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +19,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {listItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
